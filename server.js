e9e4897330e25885c7dec2de9f1e0ee7a4cbbcdd
Tejas Shripal <tejasshripal@gmail.com>
2017-08-16 19:10:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'A1.html'));
+    res.sendFile(path.join(__dirname, 'A1.html'));
 });
 
 app.get('/article-two',function(req, res) {

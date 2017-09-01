65a8c7ee2265877d62cc089317bbc10e896ea534
Tejas Shripal <tejasshripal@gmail.com>
2017-09-01 15:04:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter=0;
+app.get('/counter' , function(req ,res) {
+   counter=counter + 1;
+   res.send(counter,tostring());
+});
+
 var articleone = {
 content: `<h1>
     How important is reforstation?
